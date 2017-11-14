ffc43e4623cdac886083790844da9c73e3a138a6
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-14 01:47:15 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+def FizzBuzz (num)
+  if num % 3 == 0 && num % 5 != 0
+    return "Fizz"
+  elsif num % 3 != 0 && num % 5 == 0
+    return "Buzz"
+  elsif num % 3 == 0 && num % 5 == 0
+    return "FizzBuzz"
+  else
+    return nil
+  end
+end
+    
+    
+    
